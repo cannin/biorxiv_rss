fa652c105e97b58276f2014bc0b1ff278d17dbd3
cannin <cannin@gmail.com>
2019-03-02 15:00:47 -0500

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-43341809-11',
+      },
+    },
+    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
